4541adda27a9eca92e34b1f95f9a6169f4a9760f
brunoalves2 <brunoalves@estudante.ufscar.br>
2025-06-20 18:44:26 -0300

    Application created with Spring Starter

---
+++ pellegrino-api/src/main/java/br/com/api/pellegrino/pellegrinoapi/PellegrinoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.pellegrino.pellegrinoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PellegrinoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PellegrinoApiApplication.class, args);
+	}
+
+}
---
+++ pellegrino-api/src/test/java/br/com/api/pellegrino/pellegrinoapi/PellegrinoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.pellegrino.pellegrinoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PellegrinoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
